6c54ff0df0dd8c733557025f2823ed256e7ac3ed
Ayman-Cyber-Security <cybersecuritystudent7@gmail.com>
2026-07-30 09:34:11 +0300

    multiplication-table-7.asm

--- multiplication-table-7.asm
+++ multiplication-table-7.asm
@@ -1,1 +1,28 @@
+include emu8086.inc 
 
+
+org 100h 
+          
+mov cx,12
+
+mov bx,7
+
+mov si,0
+                   
+print_MulNumber:
+
+inc si
+mov ax,si           ; we cannot use ax alone as the counter because mul overwrites ax
+                    ; with the multiplication result each time, so we use si to keep
+                    ; a clean, untouched counter and copy it into ax before each mul
+mul bx
+
+call print_num
+printn 
+
+loop print_MulNumber
+
+
+
+DEFINE_PRINT_NUM
+DEFINE_PRINT_NUM_UNS
